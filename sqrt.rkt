b2417109455e68f7e0bb3a37a4538a5fb53c41e2
kot <vaskir@gmail.com>
2011-10-06 08:53:10 +0400

    add sqrt.rkt

---
+++ sqrt.rkt
@@ -0,0 +1,15 @@
+#lang scheme
+
+(define (sqrt x precision)
+  (define (avg x y) 
+    (/ (+ x y) 2))
+  (define (good-enough? guess)
+    (define diff (- x (* guess guess)))
+      (< (abs diff) precision))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (avg (/ x guess) guess)))) 
+  (sqrt-iter 1))
+  
+(sqrt 2.0 0.000000000000001)
